186d8b9f5dcad4fe18dfe49234fa181eb0012194
Gustavo Santana <gumadeiras@gmail.com>
2019-09-28 19:03:45 -0300

    lab05

--- lab05/inttst/inttst_counter.c
+++ lab05/inttst/inttst_counter.c
@@ -18,6 +18,7 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
     You can also obtain a copy of the GNU General Public License
+    at <http://www.gnu.org/licenses>.
 
 */
 
@@ -32,7 +33,6 @@ int main(int argc,char * argv[])
 {
         unsigned char c;
         struct pollfd pfd;
-        struct pollfd pfd_touch;
 
         if((pfd.fd=open("/sys/class/gpio/gpio6/value",O_RDONLY)) < 0)
         {
@@ -40,38 +40,28 @@ int main(int argc,char * argv[])
                 return -1;
         }
 
-        if((pfd_touch.fd=open("/sys/class/gpio/gpio62/value",O_RDONLY)) < 0)
-        {
-                perror("Opening gpio62:");
-                return -1;
-        }
-
         /* Clear old values */
         read(pfd.fd,&c,1);
-        read(pfd_touch.fd,&c,1);
 
         pfd.events=POLLPRI;
-        pfd_touch.events=POLLPRI;
 
-        puts("Waiting for interrupt... (button, then touch)");
+        puts("Waiting for interrupt...");
 
-        pputs("/sys/class/gpio/gpio6/edge","falling");
-        pputs("/sys/class/gpio/gpio62/edge","both");
+        pputs("/sys/class/gpio/gpio6/edge","both");
 
-        int interrupts_b = 0;
-        int interrupts_t = 0;
+        int interrupts = 0;
         while(1){
+            // int pollreturn = 0;
+            // pollreturn = 
             poll(&pfd,1,-1);
             lseek(pfd.fd,0,SEEK_SET);
             read(pfd.fd,&c,1);
-            interrupts_b++;
-            printf("button %d %d\n",interrupts_b,(int) (c == '1'));
-
-            poll(&pfd_touch,1,-1);
-            lseek(pfd_touch.fd,0,SEEK_SET);
-            read(pfd_touch.fd,&c,1);
-            interrupts_t++;
-            printf("touch %d %d\n",interrupts_t,(int) (c == '1'));
+            // if(pollreturn > 0)
+            // {
+            interrupts++;
+            // }
+            // printf("interrupts: %d", interrupts);
+            printf("%d %d\n",interrupts,(int) (c == '1'));
         }
             
         pputs("/sys/class/gpio/gpio6/edge","none");

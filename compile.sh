a389fe6e4d45cec184027f36a0ef4d5cc0bf94bb
Gustavo Santana <gumadeiras@gmail.com>
2019-09-28 16:42:46 -0300

    update

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-# export intel studio things
+# export target things
 export DEVKIT=/opt/iot-devkit/devkit-x86 
 export PATH=$PATH:$DEVKIT/sysroots/x86_64-pokysdk-linux/usr/bin/i586-poky-linux 
 export CROSS_COMPILE=i586-poky-
